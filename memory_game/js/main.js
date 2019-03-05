63b19386c5a3b202ec69864e728139800e620fd4
nakovskii <laeeq.naqvi@newbuzzword.com>
2019-03-04 21:15:55 -0500

    created js file

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
 // wdi memory game
 
-let cardOne = "queen";
-let cardTwo = "queen";
+
+
 let cardThree = "king";
 let cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardThree);
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " +cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " +cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0]==cardsInPlay[1]){
+    alert("You found a match!");
+  } else {
+    alert("Sorry. Try again");
+  }
+}
