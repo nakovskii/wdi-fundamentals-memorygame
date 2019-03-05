3ee1137492910fbdee1048ae58c7db363402db89
nakovskii <laeeq.naqvi@newbuzzword.com>
2019-03-04 21:54:00 -0500

    created js file

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -12,19 +12,21 @@ let cardFour = "king";
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " +cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " +cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0]==cardsInPlay[1]){
-    alert("You found a match!");
+function checkForMatch(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
   } else {
-    alert("Sorry. Try again");
+    console.log("Sorry, try again.");
   }
+} // end function
+
+
+
+function flipCard(cardId){
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+  return"User flipped " + cards[cardId];
 }
+
+flipCard(0);
+flipCard(2);
